226046f6ff4127a9a14291e99f8bfd593463bb9b
DanielPRech7 <daniel.prech19@gmail.com>
2025-10-05 01:34:12 -0300

    teste4

--- beats/core/apps.py
+++ beats/core/apps.py
@@ -4,5 +4,5 @@ from django.utils.translation import gettext_lazy as _
 
 class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'spotify2.core'
+    name = 'beats.core'
     verbose_name = _('core')
